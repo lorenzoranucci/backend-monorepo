b8505d9d2d171d3b2f1b01908bb4bf52976e0cb3
Lorenzo Franco Ranucci <lorenzofranco.ranucci@gmail.com>
2025-11-21 15:45:49 +0100

    Use an external dep

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/lorenzoranucci/backend-monorepo
 go 1.25.0
 
 ignore ./node_modules
+
+require github.com/caarlos0/env/v11 v11.3.0
--- quote-domain/hello-world-api/main.go
+++ quote-domain/hello-world-api/main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/caarlos0/env/v11"
 	"github.com/lorenzoranucci/backend-monorepo/quote-domain/shared/go-lib/printer"
 	http2 "github.com/lorenzoranucci/backend-monorepo/shared/go-lib/http"
 )
+
+type Config struct {
+	Version string `env:"Version"`
+}
+
+func main() {
+	cfg := &Config{}
+	if err := env.ParseWithOptions(cfg, env.Options{}); err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/hello", helloWorldHandler)
+	fmt.Println("Starting server on :8080...")
+	http2.StartServer()
+}
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprintln(w, printer.PrintMessage("Hello, World!!!"))
 	if err != nil {
 		return
 	}
-}
-
-func main() {
-	http.HandleFunc("/hello", helloWorldHandler)
-	fmt.Println("Starting server on :8080...")
-	http2.StartServer()
 }
